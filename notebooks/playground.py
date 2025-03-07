f7488feed290df6b42faeee60d56778ff4fcc5d3
acbass49 <acbass49@gmail.com>
2025-03-07 14:37:43 -0600

    creating and testing a make crosstabs function

--- notebooks/playground.py
+++ notebooks/playground.py
@@ -1,8 +1,10 @@
 # Load in Packages
-from survey_tools import tabs, rake_weight, recode, get_names
+from survey_tools import tabs, rake_weight, recode, get_names, make_interaction
 import pandas as pd
 import numpy as np
 import os
+
+# pip install -e .
 
 link = 'https://csed.byu.edu/00000183-a4c5-d2da-abe3-feed7be30001/2021data'
 data = pd.read_stata(link)
@@ -45,4 +47,77 @@ get_names(data,'age')
 
 data = make_interaction(data, 'gender', 'newsint_rc')
 
-tabs(data, 'religpew', 'genderXnewsint_rc',dropna=False)
+tabs(data, 'religpew', 'genderXnewsint_rc', display="column")
+
+def make_tabs(data:pd.DataFrame, vars:list[str], demos:list[str], display:str = "column", wts:str = None, to_excel: str = None):
+    '''
+    Create crosstabs of variables in vars by demographics in demos
+    
+    Required Arguments:
+        data: `pd.DataFrame` - the data to be used
+        vars: `list[str]` - the variables to be crosstabbed
+        demos: `list[str]` - the demographics to be crosstabbed by
+        display: `str` - how to display the crosstabs, either `column` or `count`. Count will give Nsize in each group. Default is column.
+    
+    Keyword Arguments:
+        wts: `str` - the name of the weights column in the data
+        to_excel: `str` - the name of the excel file to save the output to
+        
+    Returns:
+        New `pandas.DataFrame` of tabulation given parameter specifications
+    '''
+    assert isinstance(data, pd.DataFrame), "data must be a pandas DataFrame"
+    assert isinstance(vars, list), "vars must be a list of strings"
+    assert isinstance(demos, list), "demos must be a list of strings"
+    assert isinstance(display, str), "display must be a string"
+    assert display in ['column', 'count'], "display must be either 'column' or 'count'"
+    assert wts is None or isinstance(wts, str), "wts must be a string"
+    assert to_excel is None or isinstance(to_excel, str), "to_excel must be a string"
+    #check if variables are in data
+    for var in vars:
+        assert var in data.columns, f"{var} not in data"
+    #check if demographics are in data
+    for demo in demos:
+        assert demo in data.columns, f"{demo} not in data"
+    #check if weights are in data
+    if wts:
+        assert wts in data.columns, f"{wts} not in data"
+    
+    #create a list of crosstabs
+    tab_s = []
+    tabs_to_excel = []
+    for var in vars:
+        var_level_list = []
+        for demo in demos:
+            if wts:
+                tab = tabs(data, var, demo, wts = wts, display = display)
+            else:
+                tab = tabs(data, var, demo, display = display)
+            #rename columns with demo name appended
+            tab.columns = [f'{demo}: ' + str(col) for col in tab.columns]
+            #rename index with var name appended
+            tab.index = [f'{var}: ' + str(ind) for ind in tab.index]
+            var_level_list.append(tab)
+        #concatenate column-wise
+        var_tabs = pd.concat(var_level_list, axis = 1)
+        if to_excel:
+            tabs_to_excel.append(var_tabs)
+            if len(vars) > 1:
+                blank = pd.DataFrame(index = ['']*4, columns = var_tabs.columns)
+                blank = blank.fillna('')
+                blank.iloc[3,:] = var_tabs.columns
+                tabs_to_excel.append(blank)
+        tab_s.append(var_tabs)
+    #concatenate row-wise
+    final_tab = pd.concat(tab_s, axis = 0)
+    #save to excel if specified
+    if to_excel:
+            if len(tabs_to_excel) == 1:
+                final_tab_to_excel = tabs_to_excel[0]
+            else:
+                final_tab_to_excel = pd.concat(tabs_to_excel, axis = 0)
+            final_tab_to_excel.to_excel(to_excel)
+    return final_tab
+
+
+make_tabs(data=data, vars=['religpew'], demos=['genderXnewsint_rc'], to_excel='~/Desktop/test.xlsx')
